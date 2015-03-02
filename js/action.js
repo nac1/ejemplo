2ff0b0c8fb878b2c9a9593973e7866550b7f1117
nac <ignacio.h.velasco@gmail.com>
2015-03-02 12:23:09 -0600

    ejm2
    eje2

--- js/action.js
+++ js/action.js
@@ -8,14 +8,33 @@ var fn = {
     init: function(){   
         alert("2");
         $('#contact').tap(fn.activa);
+        $('#bot').tap(fn.boton);
         
     },
     activa:function(){
     alert("3");
     // Vibrate for 3 seconds
     navigator.vibrate(3000);
+    navigator.notification.beep(2);
  
-   }
+   },
+
+    boton:function(){
+    alert("4");  
+        navigator.notification.alert(
+           'Peligro' ,
+            fn.alertdimissed,
+            'Warning',
+            'Aceptar'         
+            
+        );
+        
+    
+    },
+    
+    alertdimissed:function(){
+    alert("realizada");
+    }
 
 };
 
